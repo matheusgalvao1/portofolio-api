63e196bed8e76b5de388f5ae1811c3db7a68d923
Matheus Galvão <matheusrigonigalvao@gmail.com>
2023-09-20 18:58:50 +0100

    working

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 from src.modules.chat import chat_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(chat_router, prefix="/chat")
 
+# CORS configuration
+origins = [
+    "http://localhost:3000",  # Allow requests from your frontend application
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def health_check():
-    return {
-        "Status":"OK",
-        "Message":"Access /docs to more information"    
-    }
-
-
-
+    return {"Status": "OK", "Message": "Access /docs to more information"}
--- src/modules/chat/__init__.py
+++ src/modules/chat/__init__.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from src.modules.chat.bot import get_answer, get_history
 
 chat_router = APIRouter(tags=["Chat"])
 
-@chat_router.get("/message/")
-async def message_endpoint(input: str):
+
+@chat_router.post("/message/")
+async def message_endpoint(input: str = Form(...)):
     output = get_answer(input)
     return output
+
 
 @chat_router.get("/history/")
 async def history_endpoint():
