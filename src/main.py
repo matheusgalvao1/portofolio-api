5a5df2efad058c2cb38cbbeb23f7528caee1559a
Matheus Galvão <matheusrigonigalvao@gmail.com>
2023-09-21 18:42:13 +0100

    Allow from any client address

--- src/main.py
+++ src/main.py
@@ -7,14 +7,9 @@ app = FastAPI()
 
 app.include_router(chat_router, prefix="/chat")
 
-# CORS configuration
-origins = [
-    "http://localhost:3000",  # Allow requests from your frontend application
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
