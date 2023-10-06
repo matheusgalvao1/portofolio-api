786c0c365627d399a18208868701d3aa658e9e05
Matheus Galvão <matheusrigonigalvao@gmail.com>
2023-10-06 13:48:43 +0100

    connected to atlas

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# portofolio-api
-
-To run in the localmachine
+# Run locally
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
+# Run on Docker
+docker compose up --build
--- src/db/mongo_connect.py
+++ src/db/mongo_connect.py
@@ -1,5 +1,6 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from urllib.parse import quote_plus
 
 
 class MongoDB:
@@ -15,7 +16,21 @@ class MongoDB:
         return cls.client[alias]
 
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27018/")
+mongo_user = os.getenv("MONGO_USER")
+mongo_pass = os.getenv("MONGO_PASS")
+mongo_uri_template = os.getenv("MONGO_URI_TEMPLATE")
+
+# Error handling: ensure the environment variables were found
+if not all([mongo_user, mongo_pass, mongo_uri_template]):
+    raise EnvironmentError(
+        "Missing one or more required environment variables (MONGO_USER, MONGO_PASS, MONGO_URI_TEMPLATE)"
+    )
+
+escaped_user = quote_plus(mongo_user)
+escaped_pass = quote_plus(mongo_pass)
+MONGO_URI = mongo_uri_template.format(escaped_user, escaped_pass)
+print("MONGO_URI:", MONGO_URI)
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27018/")
 
 
 async def connect_to_mongo():
